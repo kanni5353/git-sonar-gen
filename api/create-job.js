6ea486ac0ee8bd5e35341bedbc80f46e629cec87
kanni5353 <145745797+kanni5353@users.noreply.github.com>
2025-11-05 02:29:55 +0530

    Update api/create-job.js with corrected content and fixed environment variable escaping

--- api/create-job.js
+++ api/create-job.js
@@ -1,19 +1,72 @@
-sh '''
-export PATH=\${SONAR_SCANNER_PATH}:\$PATH
-sonar-scanner -Dsonar.login=\$SONAR_TOKEN
+// api/create-job.js
+const JENKINS_URL = process.env.JENKINS_URL || "http://13.61.15.150:8080";
 
-# Other content ...
+function generateJobConfig() {
+    return `
+    <project>
+      <actions>
+        <jenkins.model.BuildDiscarderProperty>
+          <strategy class="hudson.model.TimedBuildDiscarder"
+                    daysToKeepStr="7"
+                    numToKeepStr="5" />
+        </jenkins.model.BuildDiscarderProperty>
+        <org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition>
+          <script>
+            pipeline {
+              agent any
+              stages {
+                stage('Checkout') {
+                  steps {
+                    checkout scm
+                  }
+                }
+                stage('SonarQube Analysis') {
+                  steps {
+                    script {
+                      // set up SonarQube server
+                      export PATH=\${SONAR_SCANNER_PATH}:\$PATH
+                      sonar-scanner -Dsonar.login=\$SONAR_TOKEN
+                    }
+                  }
+                }
+                stage('Scan Results') {
+                  steps {
+                    script {
+                      export SONAR_HOST_URL=\${SONAR_HOST_URL}
+                      export SONAR_AUTH_TOKEN=\${SONAR_AUTH_TOKEN}
+                      export SONAR_PROJECT_KEY=
+                        $(grep '^sonar.projectKey=' sonar-project.properties | cut -d'=' -f2)
+                      export MONGO_URI=\${MONGO_URI}
+                      export MONGO_DB=\${MONGO_DB}
+                      export MONGO_COLLECTION=\${SUGGESTION_COLLECTION}
+                      export GEMINI_API_KEY=\${GEMINI_API_KEY}
+                      export SONAR_JSON="\${WORKSPACE}/temp_results/sonar_results.json"
+                    }
+                  }
+                }
+              }
+            }
+            post {
+              always {
+                script {
+                  // publish results
+                  if (fileExists(SONAR_JSON)) {
+                    // handle publishing SonarQube results
+                  }
+                }
+              }
+            }
+          </script>
+        </org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition>
+      </actions>
+    </project>`;
+}
 
-export SONAR_HOST_URL=\${SONAR_HOST_URL}
-export SONAR_AUTH_TOKEN=\${SONAR_AUTH_TOKEN}
-export SONAR_PROJECT_KEY=\$(grep
-export MONGO_URI=\${MONGO_URI}
-export MONGO_DB=\${MONGO_DB}
-export MONGO_COLLECTION=\${SUGGESTION_COLLECTION}
-export GEMINI_API_KEY=\${GEMINI_API_KEY}
-export SONAR_JSON="\${WORKSPACE}/temp_results/sonar_results.json"
+function getCrumb() {
+    return ''; // your logic here to retrieve crumb
+}
 
-# More content...
-
-export SONAR_JSON="\${WORKSPACE}/temp_results/sonar_results.json"
-'''
+function handler(req, res) {
+    const jobConfig = generateJobConfig();
+    // Logic to create/update the job in Jenkins
+}
